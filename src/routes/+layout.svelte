3b446b2c2088f7e108164613352dfc2f1bfe8564
Brandon-Ritchie <ritchietrumpet@gmail.com>
2023-09-22 09:13:26 -0400

    feat: Initial Ionic Framework Setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { setupIonicBase } from 'ionic-svelte';
+
+	/* Theme variables */
+	import '../theme/ionic-theme.css';
+
+	/* load and register all components - you can also import separately to code split */
+	// import 'ionic-svelte/components/all';
+	import 'ionic-svelte/components/ion-app';
+	import 'ionic-svelte/components/ion-button';
+
+	/* run base configuration code from ionic/core */
+	setupIonicBase();
+</script>
+
+<ion-app>
+	<slot />
+</ion-app>
